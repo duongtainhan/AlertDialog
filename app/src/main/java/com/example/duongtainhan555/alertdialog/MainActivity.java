30c18758a526ae3b5006326f511a4a8a83392223
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-07-24 18:50:10 +0700

    Alert Dialog

--- app/src/main/java/com/example/duongtainhan555/alertdialog/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/alertdialog/MainActivity.java
@@ -1,15 +1,38 @@
 package com.example.duongtainhan555.alertdialog;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button btnAlertDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Anh xa
+        btnAlertDialog = findViewById(R.id.btnAlertDialog);
+
+        btnAlertDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+                alertDialog.setTitle("Ban co muon tat hop thoai hay k?");
+                alertDialog.setMessage("Hay xac nhan ben duoi");
+                alertDialog.setIcon(R.mipmap.ic_launcher);
+                alertDialog.setPositiveButton("Co", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this,"Ban da chon co",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alertDialog.show();
+            }
+        });
     }
 }
